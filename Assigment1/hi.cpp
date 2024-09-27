2527e579892b2dd2260784decef4f7bfb7115615
Artem Belan <st135609@student.spbu.ru>
2024-09-27 12:18:24 +0300

    Test

---
+++ Assigment1/hi.cpp
@@ -0,0 +1,13 @@
+/* Artem Belan
+ st135609@student.spbu.ru
+ task1_hello_world_2.0
+ */
+
+
+#include <iostream>
+#include<string>
+using namespace std;
+string x;
+void hi(string x){
+cout<<"Hello,"<<x<<"!"<<endl;
+}
---
+++ Assigment1/main.cpp
@@ -0,0 +1,24 @@
+/* Artem Belan
+ st135609@student.spbu.ru
+ task1_hello_world_2.0
+ */
+#include <iostream>
+#include <string>
+#include "hi.h"
+using namespace std;
+int main(){
+      string a;
+      hi("world");
+      while(true)   {
+           cin>>a;
+           if(a != "0"){
+                hi(a);
+                }
+                else break;
+          }
+return 0;
+}                
+
+
+
+
