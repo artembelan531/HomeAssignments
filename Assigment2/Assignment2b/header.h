67e5d503ede98a598f95d8f1ef0ae0a94395e1c1
Artem Belan <st135609@student.spbu.ru>
2024-10-10 22:00:51 +0300

    I added header

---
+++ Assigment2/Assignment2b/header.h
@@ -0,0 +1,9 @@
+/* Belan Artem Vladislavovich
+./A2B st135609@student.spbu.ru*/
+#include <iostream>
+#ifndef HEADER_H
+#define HEADER_H
+
+double function(double num_1, double num_2, char oper);
+
+#endif
